68fb0619dad5875e60761916f86add13a404941f
Mahmoud Ghanem <moghanem345@gmail.com>
2023-08-06 19:21:15 +0300

    create a customer and Invoice Models

---
+++ app/Http/Controllers/InoviceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Inovice;
+use App\Http\Requests\StoreInoviceRequest;
+use App\Http\Requests\UpdateInoviceRequest;
+
+class InoviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInoviceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Inovice $inovice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Inovice $inovice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInoviceRequest $request, Inovice $inovice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Inovice $inovice)
+    {
+        //
+    }
+}
