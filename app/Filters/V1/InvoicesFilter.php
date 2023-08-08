551ed667b31739ba0ff22b02c6971669bfc926eb
Mahmoud Ghanem <moghanem345@gmail.com>
2023-08-08 21:04:55 +0300

    include related data

---
+++ app/Filters/V1/InvoicesFilter.php
@@ -0,0 +1,53 @@
+<?php 
+
+namespace App\Filters\V1;
+
+use Illuminate\Http\Request;
+use App\Filters\ApiFilter;
+
+class InvoicesFilter extends ApiFilter {
+    protected $safeParams = [
+        'postalCode' => ['eq', 'gt', 'lt'], 
+        'type' => ['eq'], 
+        'email' => ['eq'], 
+        'address' => ['eq'], 
+        'city' => ['eq'], 
+        'state' => ['eq'], 
+        'postalCode' => ['eq', 'gt', 'lt']
+    ];
+
+    protected $columnMap = [
+        'postalCode' => 'postal_code'
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=', 
+        'lt' => '<', 
+        'gt' => '>', 
+        'gte' => '>=', 
+        'lte' => '<=', 
+    ];
+
+    public function transform(Request $request) {
+        $eloQuery = [];
+
+        foreach ($this->safeParams as $parm => $operators) {
+            $query = $request->query($parm);
+
+            if(!isset($query)) {
+                continue;
+            } 
+
+            $columnMap = $this->columnMap[$parm] ?? $parm;
+
+            foreach ($operators as $operator) {
+                if(isset($query[$operator])) {
+                    $eloQuery[] = [$columnMap, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+        }
+
+        return $eloQuery;
+    }
+
+}
